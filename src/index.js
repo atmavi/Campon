06eb4d68f407ed7d0dd724d113b408bf500f7964
atmavi <atmavi0001@gmail.com>
2020-08-18 11:47:23 +0800

    Toggle functionality added to menu-btn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
 
+menuBtn.onclick = toggleMenu;
+
 function toggleMenu() {
-   menuBtn.classList.toggle('hidden');
+   menuBtn.classList.toggle('text-2xl');
+   menuBtn.innerHTML = menuBtn.innerHTML === '×'
+      ? 'Menu' : '&#215';
+
+   document.querySelector('.nav-links').classList.toggle('hidden');
 }
